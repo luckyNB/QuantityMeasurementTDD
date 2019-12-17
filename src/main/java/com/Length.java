3715bcfdabc7244d93e1e37807fb4e74d0758541
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-17 14:14:21 +0530

    refactored code to avoid DRY principles violation by creating ENUM

--- src/main/java/com/Length.java
+++ src/main/java/com/Length.java
@@ -1,17 +1,22 @@
 package com;
 
-public class Inches {
+public class Length {
     private final double value;
+    private  Unit unit;
 
+    enum Unit{
+    FEET,INCH;
+}
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Inches inches = (Inches) o;
+        Length inches = (Length) o;
         return Double.compare(inches.value, value) == 0;
     }
 
-    public Inches(double value) {
+    public Length(  Unit unit,double value) {
         this.value = value;
+        this.unit = unit;
     }
 }
--- src/test/java/com/QuantityMeasurementTest.java
+++ src/test/java/com/QuantityMeasurementTest.java
@@ -7,36 +7,35 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given0feetAnd0feetIfEqual_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(0.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,0.0);
         Assert.assertEquals(feet1, feet2);
     }
 
     @Test
     public void givenDifferentFeetValues_ShouldReturnFalse() {
-        Feet feet1 = new Feet(0.0);
-        Feet feet2 = new Feet(1.0);
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenOneNUllValueShouldReturnFalse() {
-        Feet feet1 = new Feet(3.0);
+        Length feet1 = new Length(Length.Unit.FEET,3.0);
         boolean result = feet1.equals(null);
         Assert.assertFalse(false);
     }
-
     @Test
     public void given0InchesAnd0InchesIfEqual_ShouldReturnTrue() {
-        Inches inche1 = new Inches(0.0);
-        Inches inche2 = new Inches(0.0);
+        Length inche1 = new Length(Length.Unit.INCH,0.0);
+        Length inche2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inche1, inche2);
     }
 
     @Test
     public void givenDifferentInchValues_ShouldReturnFalse() {
-        Inches inch1 = new Inches(0.0);
-        Inches inch2 = new Inches(1.0);
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
         Assert.assertNotEquals(inch1, inch2);
     }
 }
