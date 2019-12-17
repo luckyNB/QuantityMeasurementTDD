7e973a0000d69ecede10f348fd41d8f86b065a73
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-17 13:47:50 +0530

    Added test cases for not equals and Null

--- src/test/java/com/QuantityMeasurementTest.java
+++ src/test/java/com/QuantityMeasurementTest.java
@@ -11,4 +11,18 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenDifferentFeetValues_ShouldReturnFalse() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
+
+    @Test
+    public void givenOneNUllValueShouldReturnFalse() {
+        Feet feet1 = new Feet(3.0);
+        boolean result = feet1.equals(null);
+        Assert.assertFalse(false);
+    }
 }
