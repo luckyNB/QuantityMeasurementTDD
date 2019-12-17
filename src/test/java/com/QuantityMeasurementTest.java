43ec1c26edb10b373175120f827f5e07039bdeaa
SarswatiPatil <dipakalagate1991@gmail.com>
2019-12-17 13:33:46 +0530

    Added code and test for checking feet variable equality

--- src/test/java/com/QuantityMeasurementTest.java
+++ src/test/java/com/QuantityMeasurementTest.java
@@ -1,4 +1,14 @@
 package com;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class QuantityMeasurementTest {
+
+    @Test
+    public void given0feetAnd0feetIfEqual_ShouldReturnTrue() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
 }
